28b5cf3f0b4a781e09edc4f98f173d3710d408a7
MihailLadyk <lp346426201@gmail.com>
2020-11-28 23:34:47 +0200

    hw

--- src/App.js
+++ src/App.js
@@ -1,63 +1,77 @@
+import { v4 } from "uuid";
+import PropTypes from 'prop-types';
+import Contacts from "./components/Contacts/Contacts";
 import React, { Component } from "react";
-import Statistics from "./Statistics";
-import FeedbackOptions from "./FeedbackOptions";
-import Section from "./Section";
-import Notification from "./Notification";
+import ContactsCreator from "./components/ContactsCreator/ContactsCreator";
+import Filter from "./components/Filter/Filter";
 
 export default class App extends Component {
   state = {
-    upvotes: 0,
-    neutral: 0,
-    downvotes: 0,
+    contacts: [],
+    filter: "",
   };
 
-  
-
-  handleVote = (option) => {
+  addContact = (text, number) => {
+    if (this.state.contacts.some((e) => e.text === text) === true) {
+      alert(`${text} is already in contacts`); return  } 
+    const contact = {
+      id: v4(),
+      text: text,
+      number: number,
+    };
     this.setState((prevState) => {
       return {
-        [option]: prevState[option] + 1,
+        contacts: [...prevState.contacts, contact],
       };
     });
   };
 
-  calaculateTotalVotes = () => {
-    return this.state.upvotes + this.state.downvotes + this.state.neutral;
+  handleFilter = (e) => {
+    this.setState({
+      filter: e.target.value,
+    });
   };
-  countPositiveFeedbackPercentage = () => {
-    return (this.state.upvotes / this.calaculateTotalVotes()) * 100;
+
+
+  deleteContact = (id) => {
+    this.setState((prevState) => {
+      return {
+        contacts: prevState.contacts.filter((contact) => {
+          if (contact.id === id) {
+            return false;
+          } else {
+            return true;
+          }
+        }),
+      };
+    });
   };
+
+
 
   render() {
-    const totalVotes = this.calaculateTotalVotes();
-    const percentage = this.countPositiveFeedbackPercentage();
+    const filteredContacts = this.state.contacts.filter((contact) =>
+      contact.text.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
     return (
       <div>
-        <Section title="">
-          <FeedbackOptions
-            options={["upvotes", "downvotes", "neutral"]}
-            onVote={this.handleVote}
-          />
-
-          <h3>Statistics</h3>
-
-          {this.state.upvotes ||
-          this.state.downvotes ||
-          this.state.neutral > 0 ? (
-            <Statistics
-              upvotes={this.state.upvotes}
-              downvotes={this.state.downvotes}
-              neutral={this.state.neutral}
-              total={totalVotes}
-              percentage={percentage}
-            />
-          ) : (
-            <Notification message={"Hallou"} />
-          )}
-        </Section>
-
-        <hr />
+        <ContactsCreator addContact={this.addContact} />
+        <Filter Filter={this.handleFilter} />
+        <h2>Contacts</h2>
+        <Contacts contact={filteredContacts} deleteContact={this.deleteContact}/>
       </div>
     );
   }
 }
+ContactsCreator.propTypes = {
+addContact: PropTypes.func
+}
+
+Filter.propTypes = {
+Filter: PropTypes.func
+  }
+
+  Contacts.propTypes = {
+   contact: PropTypes.array,
+   deleteContact: PropTypes.func
+      }
